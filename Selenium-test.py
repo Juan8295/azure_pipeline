d3622b27e92fdc9ed268f50c992db1195e7ecd3b
Juan Goez <juan.goez.cordoba@devoteam.com>
2022-03-11 16:50:00 +0100

    add_selenium_test

--- Selenium-test.py
+++ Selenium-test.py
@@ -11,7 +11,7 @@ import unittest, time, re
 
 class AddEditTest(unittest.TestCase):
     def setUp(self):
-        s = Service("/home/juan/Downloads/chromedriver_linux64/chromedriver")
+        s = Service("/home/azureuser/Downloads/chromedriver")
         self.driver = webdriver.Chrome(service=s)
         self.driver.implicitly_wait(30)
         self.base_url = "http://localhost:8084/petclinic"
